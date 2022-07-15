387ee751df60c5238d9c17ff46480c14a992ba41
projeksimple <projeksederhanadev@gmail.com>
2022-07-15 08:16:04 +0700

    first-commit

---
+++ penghitung_karakter.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+string userinput;
+
+cout << "Input:";getline(cin,userinput);
+cout << "length:" <<userinput.length()<<" words";
+cout << endl;
+cout << "output:"<<userinput;
+
+    cin.get();
+    return 0;
+}
